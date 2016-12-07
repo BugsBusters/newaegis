503723eb091c5ae0d52ed80ad2dfbc54a277f062
Andrea Civita <andreacivita994@gmail.com>
2016-12-07 15:27:39 +0100

    VISUALIZIONE FUNZIONATE
    ORA FUNZIONANO GLI ALBERELLI

---
+++ public/adminBundle/js/nodo.js
@@ -0,0 +1,8 @@
+function setPosition(element, x, y, map) {
+    console.log(map.offset());
+    element.offset({
+        left: map.offset().left + (x * map.width()),
+        top:  map.offset().top + (y * map.height())
+    })
+
+}
